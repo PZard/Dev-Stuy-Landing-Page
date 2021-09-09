7317160df5a13538b6be4efdc6474320a901de7c
Pedro Zardin <pezazagui@gmail.com>
2021-09-09 09:42:03 -0300

    Merge manual das alterações do Alcalde

--- js/script.js
+++ js/script.js
@@ -1,22 +1,54 @@
-function addFadeIn (element) {
-    $(document).ready(function() {
-        var alturaViewport  = $(window).height();
-        var alturaElemento  = $(element).height();
-        var diferenca       = alturaElemento - alturaViewport;
-        var chegou          = false;
-    
-        $(window).scroll(function() {
-    
-            var posicaoScroll   = $(window).scrollTop();
-    
-            if(!chegou){
-                if (posicaoScroll >= (diferenca)) {
-                    $(element).addClass("fadein");
-                    chegou = true;
-                }
-            }
-        });
-    });
+var imgs = [];
+var slider;
+var total = 3;
+var cImg;
+var maxImg;
+var temp;
+
+//informações do caminho até as imagens (salvas como nome+numero)
+var tipo = ".png";
+var path = "images/slider";
+var nome = "/s";
+
+function preLoad() {
+    var s = 1;
+    for(var i = 0; i < total; i++){
+        imgs[i] = new Image();
+        imgs[i].src = path+nome+s+tipo;
+        s++;
+    }
 }
 
-addFadeIn('.qualities-items');
+function load() {
+    slider.src = imgs[cImg].src;
+}
+
+function get() {
+    preLoad();
+    cImg = 0;
+    maxImg = imgs.length - 1;
+    slider = document.getElementById("circulo");
+    load();
+    temp = 0;
+}
+
+function anima() {
+    temp++;
+    if (temp >= 5000){
+        temp = 0;
+        slide(1);
+    }
+}
+
+function slide(side) {
+    temp = 0;
+    cImg += side;
+    if (cImg > maxImg){
+        cImg = 0;
+    } else if (cImg < 0){
+        cImg = maxImg;
+    }
+    load(cImg);
+}
+
+window.addEventListener("load",get);
